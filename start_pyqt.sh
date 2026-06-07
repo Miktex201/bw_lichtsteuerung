9ab6c8be7592f95dcb2e87ba8da923274a2cec01
Miktex201 <miklas-kohn@web.de>
2026-06-07 12:59:20 +0200

    autostart

--- start_pyqt.sh
+++ start_pyqt.sh
@@ -4,12 +4,13 @@ set -eu
 cd "$(dirname "$0")"
 
 export LICHT_DMX_ENABLED="${LICHT_DMX_ENABLED:-1}"
-export LICHT_DMX_DEVICE="${LICHT_DMX_DEVICE:-/dev/ttyUSB1}"
+export LICHT_DMX_DEVICE="${LICHT_DMX_DEVICE:-/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG00QFIM-if00-port0}"
 export LICHT_DMX_FPS="${LICHT_DMX_FPS:-44}"
 export LOGO_GPIO_ENABLED="${LOGO_GPIO_ENABLED:-1}"
 export LOGO_GPIO_RED="${LOGO_GPIO_RED:-18}"
 export LOGO_GPIO_GREEN="${LOGO_GPIO_GREEN:-10}"
 export LOGO_GPIO_BLUE="${LOGO_GPIO_BLUE:-17}"
+export PYQT_API_BASE="${PYQT_API_BASE:-http://127.0.0.1:8080}"
 
 if [ -S /tmp/.X11-unix/X0 ]; then
     export DISPLAY="${DISPLAY:-:0}"
@@ -20,4 +21,9 @@ else
     export QT_QPA_PLATFORM="${QT_QPA_PLATFORM:-linuxfb}"
 fi
 
-exec /usr/bin/python3 pyqt_app.py --fullscreen
+PYQT_ARGS="--fullscreen"
+if [ "${PYQT_USE_WEBSERVER:-0}" = "1" ]; then
+    PYQT_ARGS="$PYQT_ARGS --use-webserver --api-base $PYQT_API_BASE"
+fi
+
+exec /usr/bin/python3 pyqt_app.py $PYQT_ARGS
--- start_pyqt_x.sh
+++ start_pyqt_x.sh
@@ -4,11 +4,17 @@ set -eu
 APP_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 export LICHT_DMX_ENABLED="${LICHT_DMX_ENABLED:-1}"
-export LICHT_DMX_DEVICE="${LICHT_DMX_DEVICE:-/dev/ttyUSB1}"
+export LICHT_DMX_DEVICE="${LICHT_DMX_DEVICE:-/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG00QFIM-if00-port0}"
 export LICHT_DMX_FPS="${LICHT_DMX_FPS:-44}"
 export LOGO_GPIO_ENABLED="${LOGO_GPIO_ENABLED:-1}"
 export LOGO_GPIO_RED="${LOGO_GPIO_RED:-18}"
 export LOGO_GPIO_GREEN="${LOGO_GPIO_GREEN:-10}"
 export LOGO_GPIO_BLUE="${LOGO_GPIO_BLUE:-17}"
+export PYQT_API_BASE="${PYQT_API_BASE:-http://127.0.0.1:8080}"
 
-exec startx /usr/bin/python3 "$APP_DIR/pyqt_app.py" --fullscreen -- :0 vt2 -nocursor
+PYQT_ARGS="--fullscreen"
+if [ "${PYQT_USE_WEBSERVER:-0}" = "1" ]; then
+    PYQT_ARGS="$PYQT_ARGS --use-webserver --api-base $PYQT_API_BASE"
+fi
+
+exec startx /usr/bin/python3 "$APP_DIR/pyqt_app.py" $PYQT_ARGS -- :0 vt2 -nocursor
